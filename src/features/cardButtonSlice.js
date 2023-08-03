8aef9a6915a39d19125cb6ce499a6d9143625d04
Janica <abagat07@gmail.com>
2023-08-03 11:44:15 -0700

    modified right div styles

--- src/features/cardButtonSlice.js
+++ src/features/cardButtonSlice.js
@@ -3,13 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 const cardButtonSlice = createSlice({
   name: 'cardButton',
   initialState: {
-    isClicked: false,
+    isClicked: true,
     restaurantInfo: null, // Set initial value to null
   },
   reducers: {
     toggleCardButton: (state, action) => {
       state.isClicked = !state.isClicked;
-      state.restaurantInfo = action.payload; // Update restaurantInfo with the payload
+      state.isClicked ? state.restaurantInfo = action.payload : state.restaurantInfo = null; // Update restaurantInfo with the payload
     },
   },
 });
